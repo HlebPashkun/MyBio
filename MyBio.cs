223e5b696a91843dac414ff08a81221fb871856a
HlebPashkun <fgascoigne3@gmail.com>
2025-06-18 16:46:20 +0300

    Create MyBio.cs

---
+++ MyBio.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string firstName = "Hleb";
+        string lastName = "Pashkun";
+
+        Console.WriteLine("First name: " + firstName);
+        Console.WriteLine("Last name: " + lastName);
+    }
+}
